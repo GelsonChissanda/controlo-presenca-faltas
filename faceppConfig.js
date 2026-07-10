487e25313c0728734187f86fb8fd77382eb2a26e
Gelson Chissanda <gelsonchissanda@gmail.com>
2026-07-10 15:20:05 +0100

    Ajustei o face++ para funcionar no expo da apple story

--- faceppConfig.js
+++ faceppConfig.js
@@ -1,6 +1,6 @@
 // Credenciais da API Face++ (console.faceplusplus.com)
-export const FACEPP_API_KEY = "XFtUqiQhT_-eT1_YdIvL4vAGZTlyIG9U";
-export const FACEPP_API_SECRET = "7v1TxGsqm04UsTYjE6VjWd489cNaMkmV";
+export const FACEPP_API_KEY ="XFtUqiQhT_-eT1_YdIvL4vAGZTlylG9U";
+export const FACEPP_API_SECRET ="7v1TxGsqm04UsTYjE6VjWd489cNaMkmV";
 
 // Nome do FaceSet (a "base de dados" de rostos da escola)
 export const FACEPP_FACESET_OUTER_ID = "controlo_presenca_alunos";
