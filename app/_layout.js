bc1c54e9bc08cb0fcd9461e129a2ef35fd12a9d5
Gelson Chissanda <gelsonchissanda@gmail.com>
2026-07-22 18:22:27 +0100

    feat: added notifications

--- app/_layout.js
+++ app/_layout.js
@@ -3,12 +3,14 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { AuthProvider } from "../context/AuthContext";
 import { ThemeProvider } from "../context/ThemeContext";
+import NotificacoesListener from "../components/NotificacoesListener";
 
 export default function RootLayout() {
   return (
     <ThemeProvider>
       <AuthProvider>
         <StatusBar style="dark" />
+        <NotificacoesListener />
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="index" />
         </Stack>
